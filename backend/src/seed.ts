8a2628c5ac3aadce12087a0ffc4109d5359c4753
adrians <adiskir@gmail.com>
2026-05-17 16:22:49 +0300

    added queen species seed data

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -1,14 +1,31 @@
 import { sql } from 'drizzle-orm';
 import { db } from './config/Database';
 import { HistoryActionType } from './DatabaseEnums';
-import { historyActionTypes } from './db/schema';
+import { historyActionTypes, queenSpecies } from './db/schema';
+import { withStatus } from './utils';
+import species from '../species.json';
 
 async function seed() {
-    await db.insert(historyActionTypes)
-        .values(Object.values(HistoryActionType).map(type => ({ type })))
-        .onDuplicateKeyUpdate({ set: { type: sql`type` } });
+    await withStatus("Seeded history_action_types", () => 
+        db.insert(historyActionTypes)
+            .values(Object.values(HistoryActionType).map(type => ({ type })))
+            .onDuplicateKeyUpdate({ set: { type: sql`type` } }
+        )
+    )
+
+    await withStatus("Seeded queen_species", () =>
+        db.insert(queenSpecies) 
+            .values(species.map(item => ({
+                knownAsName:    item.knownAsName,
+                scientificName: item.scientificName,
+                lifeExpectancy: item.lifeExpectancy,
+                description:    item.description,
+                behavior:       item.behavior,
+                preferences:    item.preferences,
+            })))
+            .onDuplicateKeyUpdate({ set: { scientificName: sql`scientificName` } })
+    )
     
-    console.log('Seeded history_action_types');
     process.exit(0);
 }
 
